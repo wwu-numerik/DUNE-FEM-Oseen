b275b23197e500f874c34f4df5f0a5620087f685
Felix Albrecht <felix@almob.(none)>
2008-10-29 19:46:52 +0100

    stokespass.hh and spinverseoperator.hh added

---
+++ dune-stokes/stokes/spinverseoperator.hh
@@ -0,0 +1,8 @@
+/** \file spinverseoperator.hh
+    \brief  spinverseoperator.hh
+ **/
+
+#ifndef SPINVERSEOPERATOR_HH
+#define SPINVERSEOPERATOR_HH
+
+#endif  // end of spinverseoperator.hh
---
+++ dune-stokes/stokes/stokespass.hh
@@ -0,0 +1,8 @@
+/** \file stokespass.hh
+    \brief  stokespass.hh
+ **/
+
+#ifndef STOKESPASS_HH
+#define STOKESPASS_HH
+
+#endif  // end of stokespass.hh
