4f2da0869a9d20c920247f9170154685628bbc3b
Felix Albrecht <felix@server.(none)>
2008-10-31 11:30:48 +0100

    minor changes
    Signed-off-by: Felix Albrecht <felix@server.(none)>

--- dune-stokes/src/parametercontainer.hh
+++ dune-stokes/src/parametercontainer.hh
@@ -6,142 +6,6 @@
 #define PARAMETERHANDLER_HH
 
 #include "dune/fem/io/parameter.hh"
-//#include <fstream>
-//#include <ostream>
-//#include <map>
-//#include "stuff.hh"
-//#include "logging.hh"
-
-///**
-// *  \brief class processing parameter file
-// *
-// *  \c ParameterHandler reads a parameter file once and stores all found values internally
-// **/
-//class ParameterHandler
-//{
-//    private:
-//        typedef std::map< std::string, std::string > MapType;
-//        MapType parameter_map_;
-//        bool status_;
-//
-//    public:
-//        /** \ Please doc me!
-//         *
-//         **/
-//        ParameterHandler( const std::string filename )
-//            :status_( false )
-//        {
-//            ParseParamFile( filename );
-//        }
-//
-//        ParameterHandler(  )
-//            :status_( false )
-//        {
-//        }
-//
-//        /** \brief function used for parametrized ctor and two-step creation
-//        **/
-//        bool ParseParamFile( const std::string filename )
-//        {
-//            std::ifstream parameter_file( filename.c_str() );
-//            if( parameter_file.is_open() )
-//            {
-//                while( !parameter_file.eof() )
-//                {
-//                    std :: string line;
-//                    std :: getline( parameter_file, line );
-//
-//                    if( line.size() == 0 )
-//                        continue;
-//
-//                    if( (line[ 0 ] != '%') && (line[ 0 ] != '#') )
-//                    {
-//                        std::string param_name = line.substr( 0, line.find_first_of(":") );
-//                        std::string value = line.substr( line.find_first_of(":")+1, line.length() );
-//                        parameter_map_[param_name] = value;
-//                    }
-//
-//                }
-//                status_ = true;
-//                parameter_file.close();
-//            }
-//            else {
-//                //LOGERROR
-//                status_ = false;
-//                std::cerr << "ERROR: file " << filename << " not found!\n";
-//            }
-//            return Ok();
-//        }
-//
-//        /**
-//         *  \brief checks, if a parameter is found in the parameterfile
-//         *  \arg const std::string name name of the parameter to be found
-//         *  \return returns true, if the parameter is found
-//         **/
-//         bool ParameterExists( const std::string name ) const
-//         {
-//             if ( !( status_ ) ) {
-//                 return false;
-//             }
-//             else {
-//                 MapType::const_iterator it = parameter_map_.find( name );
-//                 if ( it != parameter_map_.end() ) {
-//                     return true;
-//                 }
-//                 else {
-//                     return false;
-//                 }
-//
-//             }
-//         }
-//
-//        /** \todo Please doc me! */
-//        template < class ReturnType >
-//        ReturnType GetParameter( const std::string name) const
-//        {
-//            if ( !( status_ ) ) {
-//                return (ReturnType) 0;
-//            }
-//            else {
-//                MapType::const_iterator it = parameter_map_.find( name ) ;
-//                if ( it != parameter_map_.end() ){
-//                    return Stuff::fromString<ReturnType>( it->second );
-//                }
-//                else {
-//                    //LogError
-//                    return (ReturnType) 0;
-//                }
-//            }
-//        }
-//
-//        /** \todo Please doc me! */
-//        void Print( LogStream &out ) const
-//        {
-//            assert( status_ );
-//            for (MapType::const_iterator it = parameter_map_.begin(); parameter_map_.end() != it; ++it){
-//                out << it->first << ":" << it->second << "\n" ;
-//            }
-//            //out << std::endl;
-//        }
-//
-//        /** \todo Please doc me! */
-//        bool Ok()
-//        {
-//            return status_;
-//        }
-//
-//        /** \todo Please doc me! */
-//        ~ParameterHandler(){}
-//
-//};
-
-///** \brief global singelton for paramhandler
-//**/
-//ParameterHandler& params()
-//{
-//    static ParameterHandler param;
-//    return param;
-//}
 
 /**
  *  \brief class containing global parameters
@@ -212,10 +76,11 @@ class ParameterContainer
             Dune::Parameter::append( argc_, argv_ );
             bool has_not_worked = false;
             if ( !( Dune::Parameter::exists( "dimension" ) ) ) {
-                has_not_worked = true;
                 std::cerr << "\nError: not all parameters found in " << parameterFilename() << "!";
                 PrintParameterSpecs( std::cerr );
                 std::cerr << "missing parameters are: dimension" << std::endl;
+                has_not_worked = true;
+
             }
             else {
                 Dune::Parameter::get( "dimension", dimension_ );
@@ -234,6 +99,9 @@ class ParameterContainer
             else {
                 Dune::Parameter::get( "polynomial_order", pol_order_ );
             }
+            if ( has_not_worked ) {
+                std::cerr << std::endl;
+            }
             return !( has_not_worked );
         }
 
@@ -243,7 +111,8 @@ class ParameterContainer
          **/
         void PrintParameterSpecs( std::ostream& out ) const
         {
-            out << "\na valid parameterfile should at least specify the following parameters:" << std::endl;
+            out << "\na valid parameterfile should at least specify the following parameters:";
+            out << "\n(copy this into your parameterfile)" << std::endl;
             out << "dimension: " << std::endl;
             out << "polynomial_order: " << std::endl;
             out << std::endl;
