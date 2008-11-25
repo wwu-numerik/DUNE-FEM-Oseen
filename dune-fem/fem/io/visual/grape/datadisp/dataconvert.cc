18bde809d65fb7700d407a93b87e2fd82285d4cb
Rene Milk <koshi@springlobby.info>
2008-11-25 13:41:08 +0100

    fixed some lingering conflicts

--- dune-fem/fem/io/visual/grape/datadisp/dataconvert.cc
+++ dune-fem/fem/io/visual/grape/datadisp/dataconvert.cc
@@ -9,11 +9,7 @@
 #include <string>
 
 #if HAVE_MPI == 1 
-<<<<<<< HEAD:dune-fem/fem/io/visual/grape/datadisp/dataconvert.cc
 #warning "Visualization does not work in parallel" 
-=======
-#error "Visualization only works without MPI" 
->>>>>>> reverts all post 1.1.1 commits except adding a new build target for suse10.3:dune-fem/fem/io/visual/grape/datadisp/dataconvert.cc
 #endif 
 
 #include <dune/common/misc.hh>
@@ -21,11 +17,8 @@
 using namespace Dune;
 
 // include definition of grid type 
-<<<<<<< HEAD:dune-fem/fem/io/visual/grape/datadisp/dataconvert.cc
 // #include <dune/grid/io/file/dgfparser/dgfgridtype.hh>
-=======
-#include <dune/grid/io/file/dgfparser/dgfgridtype.hh>
->>>>>>> reverts all post 1.1.1 commits except adding a new build target for suse10.3:dune-fem/fem/io/visual/grape/datadisp/dataconvert.cc
+
 
 // include data reading 
 #include <dune/fem/io/visual/grape/datadisp/printhelp.cc>
@@ -33,7 +26,6 @@ using namespace Dune;
 // uses readtuple data instead of readiotupledata.
 #include <dune/fem/io/visual/grape/datadisp/readtupledata.cc>
 #include <dune/fem/io/visual/grape/datadisp/readioparams.cc> 
-<<<<<<< HEAD:dune-fem/fem/io/visual/grape/datadisp/dataconvert.cc
 #include <dune/fem/misc/mpimanager.hh>
 
 int main(int argc, char **argv)
@@ -41,12 +33,7 @@ int main(int argc, char **argv)
   MPIManager::initialize(argc,argv);
   try {
     Parameter::append(argc,argv);
-=======
 
-int main(int argc, char **argv)
-{
-  try {
->>>>>>> reverts all post 1.1.1 commits except adding a new build target for suse10.3:dune-fem/fem/io/visual/grape/datadisp/dataconvert.cc
     if (argc < 2)
     {
       print_help(argv[0]);
