c36fc86f6dbeceaf6141887c883ae9fdb456b1e4
Felix Albrecht <felix.albrecht@math.uni-muenster.de>
2009-01-26 11:34:42 +0100

    change selective problem code
    Conflicts:
    
    	dune-stokes/src/analyticaldata.hh
    	dune-stokes/src/dune_stokes.cc
    	dune-stokes/src/pressure.hh
    	dune-stokes/src/test.param
    	dune-stokes/src/velocity.hh

--- dune-stokes/src/pressure.hh
+++ dune-stokes/src/pressure.hh
@@ -131,11 +131,17 @@ inline void Pressure< PressureTraitsImp  >::evaluate( const DomainType& arg, Ran
     // play save
     assert( arg.dim() == 2 );
     assert( ret.dim() == 1 );
-    // some computations
     double x1 = arg[0];
     double x2 = arg[1];
-    // return
+
+#ifdef SIMPLE_PROBLEM
+    ret[0] = x1*x2;
+#elif defined(CONSTANT_PROBLEM)
+    ret[0] = 9;
+#else
     ret[0] = 2.0 * std::exp( x1 ) * std::sin( x2 );
+#endif
+
 }
 
 /**
