0eec65f1b0c05e85483d5ddd790e1c465befc660
Felix Albrecht <felix.albrecht@math.uni-muenster.de>
2009-03-06 16:40:26 +0100

    added exact solution plotting in matlab

--- dune-stokes/src/plotting.m
+++ dune-stokes/src/plotting.m
@@ -4,7 +4,9 @@ for i = 1:length( x )
     for j = 1:length( x )
         x_1 = x( i );
         x_2 = x( j );
-        p_exact( i, j ) = exp( x_1 ) * x_2 * sin( x_2 );
+        p_exact( i, j ) = 2 * exp( x_1 ) * sin( x_2 );
+        u_1( i, j ) = -1 * exp( x_1 ) * ( x_2 * cos( x_2 ) + sin( x_2 ) );
+        u_2( i, j ) = exp( x_1 ) * x_2 * sin( x_2 );
     end
 end
 
@@ -19,4 +21,18 @@ title('exact pressure');
 xlabel('-1 < x < 1');
 ylabel('-1 < y < 1');
 legend('exact pressure');
+figure( 'Position', [1 1 screen_size(3) screen_size(4)],...
+        'Name', 'exact velocity 1' );
+mesh( x, x, u_1 );
+title('exact velocity 1');
+xlabel('-1 < x < 1');
+ylabel('-1 < y < 1');
+legend('exact velocity 1');
+figure( 'Position', [1 1 screen_size(3) screen_size(4)],...
+        'Name', 'exact velocity 2' );
+mesh( x, x, u_2 );
+title('exact velocity 2');
+xlabel('-1 < x < 1');
+ylabel('-1 < y < 1');
+legend('exact velocity 2');
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %
