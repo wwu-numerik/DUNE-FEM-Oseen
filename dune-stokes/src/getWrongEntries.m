6b7f57a280d435c93e54c4afa86072880f91a147
Felix Albrecht <felix@server.(none)>
2009-04-14 23:14:05 +0200

    changed matlab skript

--- dune-stokes/src/getWrongEntries.m
+++ dune-stokes/src/getWrongEntries.m
@@ -5,12 +5,14 @@ numberOfWrongEntries = 0;
 if ( size_felix == size_mirko )
     for i = 1:size_felix(1)
         for j = 1:size_felix(2)
-            tmp = felix( i, j ) - mirko( i, j );
+            felix_i_j = felix( i, j );
+            mirko_i_j =  mirko( i, j );
+            tmp = felix_i_j - mirko_i_j;
             if ( abs( tmp > 1e-10 ) )
                 numberOfWrongEntries = numberOfWrongEntries + 1;
                 y( numberOfWrongEntries, 1 ) = i;
                 y( numberOfWrongEntries, 2 ) = j;
-                fprintf( 1, '%2d, %2d: %6f\n', i - 1, j - 1, tmp );
+                fprintf( 1, '%2d, %2d: %6f - %6f = %6f\n', i - 1, j - 1, felix_i_j, mirko_i_j, tmp );
             end
         end
     end
