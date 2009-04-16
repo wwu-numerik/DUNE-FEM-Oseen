2d07d86277569ace1950c31245c6da9da9af5721
Felix Tobias Albrecht <f_albr01@BARNEY.uni-muenster.de>
2009-04-16 17:59:00 +0200

    fixed misuse of abs() in getWrongEntries

--- dune-stokes/src/getWrongEntries.m
+++ dune-stokes/src/getWrongEntries.m
@@ -1,17 +1,13 @@
 function y = getWrongEntries( felix, mirko )
 size_felix = size( felix );
 size_mirko = size( mirko );
-numberOfWrongEntries = 0;
 if ( size_felix == size_mirko )
     for i = 1:size_felix(1)
         for j = 1:size_felix(2)
             felix_i_j = felix( i, j );
             mirko_i_j =  mirko( i, j );
             tmp = felix_i_j - mirko_i_j;
-            if ( abs( tmp > 1e-10 ) )
-                numberOfWrongEntries = numberOfWrongEntries + 1;
-                y( numberOfWrongEntries, 1 ) = i;
-                y( numberOfWrongEntries, 2 ) = j;
+            if ( abs( tmp ) > 1e-10 )
                 fprintf( 1, '%2d, %2d: is %6f but should be %6f\n', i - 1, j - 1, felix_i_j, mirko_i_j );
             end
         end
