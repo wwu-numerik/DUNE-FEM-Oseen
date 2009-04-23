f67813efae9afd83abc78978bbd7baa6fc0ad85c
Rene Milk <r_milk01@DUNE-PROJECT.uni-muenster.de>
2009-04-23 15:53:24 +0200

    set polorder to 1, re-ref dbg-stream

--- dune-stokes/stokes/stokespass.hh
+++ dune-stokes/stokes/stokespass.hh
@@ -362,8 +362,8 @@ MMatrix.reserve();
 #ifndef NLOG
             // logging stuff
             Logging::LogStream& infoStream = Logger().Info();
-//            Logging::LogStream& debugStream = Logger().Info();
-            Logging::LogStream& debugStream = Logger().Dbg();
+            Logging::LogStream& debugStream = Logger().Info();
+//            Logging::LogStream& debugStream = Logger().Dbg();
             bool entityOutput = false;
             bool intersectionOutput = false;
             const int outputEntity = 0;
