d6bfc5012ee8466cf6960666ddbf33dafb26a7a4
Felix Albrecht <felix@almob.(none)>
2009-07-11 15:39:34 +0200

    disabled the mpi warning

--- dune-fem/fem/space/common/communicationmanager.hh
+++ dune-fem/fem/space/common/communicationmanager.hh
@@ -1,32 +1,32 @@
 #ifndef DUNE_COMMUNICATION_MANAGER_HH
 #define DUNE_COMMUNICATION_MANAGER_HH
 
-//- system includes 
+//- system includes
 #include <iostream>
-#include <map> 
+#include <map>
 #include <vector>
 
-//- Dune includes  
+//- Dune includes
 #include <dune/common/mpihelper.hh>
 #include <dune/grid/common/datahandleif.hh>
 #include <dune/grid/common/grid.hh>
 #include <dune/fem/space/common/commoperations.hh>
 
-// include ALUGrid to check whether the 
-// parallel version is avaiable 
-#if defined ENABLE_ALUGRID 
+// include ALUGrid to check whether the
+// parallel version is avaiable
+#if defined ENABLE_ALUGRID
 #include <dune/grid/alugrid.hh>
 #endif
 
-#if ALU3DGRID_PARALLEL 
-#define USE_CACHED_COMM_MANAGER 
-#else 
-#ifndef NDEBUG 
+#if ALU3DGRID_PARALLEL
+#define USE_CACHED_COMM_MANAGER
+#else
+#ifndef NDEBUG
 #if HAVE_MPI == 0
-  #warning "HAVE_MPI == 0, therefore default CommunicationManager is used!"
-#elif !ALU3DGRID_PARALLEL 
+  //#warning "HAVE_MPI == 0, therefore default CommunicationManager is used!"
+#elif !ALU3DGRID_PARALLEL
   #warning "No Parallel ALUGrid found, using default CommunicationManager!"
-#endif 
+#endif
 #endif
 #endif
 
@@ -34,15 +34,15 @@
 #include "cachedcommmanager.hh"
 #endif
 
-//- Dune-fem includes 
+//- Dune-fem includes
 #include <dune/fem/storage/singletonlist.hh>
 #include <dune/fem/space/common/commoperations.hh>
 #include <dune/fem/space/common/arrays.hh>
 
 namespace Dune
 {
-  
-/** @addtogroup Communication Communication 
+
+/** @addtogroup Communication Communication
     @{
 **/
 
@@ -52,10 +52,10 @@ namespace Dune
    *         method
    */
   template< class Space >
-  class DefaultCommunicationManager 
+  class DefaultCommunicationManager
   {
   public:
-    typedef Space SpaceType; 
+    typedef Space SpaceType;
 
   protected:
     typedef typename SpaceType :: GridPartType GridPartType;
@@ -65,7 +65,7 @@ namespace Dune
 
     const InterfaceType interface_;
     const CommunicationDirection dir_;
-    
+
   public:
     //! constructor taking space, but here only storing gridPart for
     //! communication
@@ -81,10 +81,10 @@ namespace Dune
   private:
     // prohibit copying
     DefaultCommunicationManager ( const DefaultCommunicationManager & );
-    
+
   public:
     /** \brief exchange data for a discrete function using the copy operation
-     *  
+     *
      *  \param  discreteFunction  discrete function to communicate
      */
     template< class DiscreteFunction >
@@ -97,7 +97,7 @@ namespace Dune
      *
      *  The used operation is derived from the type of the op-pointer. The
      *  actual pointer is not used.
-     *  
+     *
      *  \param      discreteFunction  discrete function to communicate
      *  \param[in]  operation         a (phony) pointer to an operation
      */
@@ -109,11 +109,11 @@ namespace Dune
       typedef typename DiscreteFunction
         :: template CommDataHandle< Operation > :: Type
         DataHandleType;
-      
+
       // on serial runs: do nothing
       if( gridPart_.grid().comm().size() <= 1 )
         return;
-    
+
       // communicate data
       DataHandleType dataHandle = discreteFunction.dataHandle( operation );
       gridPart_.communicate( dataHandle, interface_ , dir_ );
@@ -121,33 +121,33 @@ namespace Dune
   };
 
 
-  
+
 #ifndef USE_CACHED_COMM_MANAGER
-  // if no ALUGrid found, supply default implementation 
-  //! \brief use Default CommunicationManager as Communication Manager 
-  template <class SpaceImp> 
-  class CommunicationManager 
-  : public DefaultCommunicationManager<SpaceImp> 
+  // if no ALUGrid found, supply default implementation
+  //! \brief use Default CommunicationManager as Communication Manager
+  template <class SpaceImp>
+  class CommunicationManager
+  : public DefaultCommunicationManager<SpaceImp>
   {
     typedef DefaultCommunicationManager<SpaceImp> BaseType;
     CommunicationManager(const CommunicationManager &);
   public:
     //! constructor taking space, but here only storing gridPart for
     //! communication
-    CommunicationManager(const SpaceImp & space,  
+    CommunicationManager(const SpaceImp & space,
         const InterfaceType interface = InteriorBorder_All_Interface,
         const CommunicationDirection dir = ForwardCommunication )
-      : BaseType(space,interface,dir) 
+      : BaseType(space,interface,dir)
     {}
   };
 
 
 
-  //! Proxy class to DependencyCache which is singleton per space 
-  class CommunicationManagerList  
+  //! Proxy class to DependencyCache which is singleton per space
+  class CommunicationManagerList
   {
-    //! communicated object interface 
-    class DiscreteFunctionCommunicatorInterface  
+    //! communicated object interface
+    class DiscreteFunctionCommunicatorInterface
     {
     protected:
       DiscreteFunctionCommunicatorInterface () {}
@@ -155,28 +155,28 @@ namespace Dune
       virtual ~DiscreteFunctionCommunicatorInterface () {}
       virtual void exchange () = 0;
     };
-    
-    //! communicated object implementation  
+
+    //! communicated object implementation
     template <class DiscreteFunctionImp>
-    class DiscreteFunctionCommunicator 
-    : public DiscreteFunctionCommunicatorInterface 
+    class DiscreteFunctionCommunicator
+    : public DiscreteFunctionCommunicatorInterface
     {
       typedef DiscreteFunctionImp DiscreteFunctionType;
       typedef typename DiscreteFunctionType :: DiscreteFunctionSpaceType DiscreteFunctionSpaceType;
-      
-      typedef CommunicationManager<DiscreteFunctionSpaceType> CommunicationManagerType; 
-    
+
+      typedef CommunicationManager<DiscreteFunctionSpaceType> CommunicationManagerType;
+
       DiscreteFunctionType& df_;
       CommunicationManagerType comm_;
-    public:  
-      //! constructor taking disctete function 
-      DiscreteFunctionCommunicator(DiscreteFunctionType& df) 
+    public:
+      //! constructor taking disctete function
+      DiscreteFunctionCommunicator(DiscreteFunctionType& df)
         : df_(df), comm_(df_.space())
       {
       }
 
-      // exchange discrete function 
-      void exchange () 
+      // exchange discrete function
+      void exchange ()
       {
         comm_.exchange(df_);
       }
@@ -186,19 +186,19 @@ namespace Dune
     typedef std::list < DiscreteFunctionCommunicatorInterface * > CommObjListType;
     CommObjListType objList_;
 
-    CommunicationManagerList(const CommunicationManagerList&); 
-  public: 
+    CommunicationManagerList(const CommunicationManagerList&);
+  public:
     //! constructor
     template <class CombinedObjectType>
-    CommunicationManagerList(CombinedObjectType& cObj) 
+    CommunicationManagerList(CombinedObjectType& cObj)
     {
       cObj.addToList(*this);
     }
 
     //! remove object comm
-    ~CommunicationManagerList() 
+    ~CommunicationManagerList()
     {
-      // delete all entries 
+      // delete all entries
       while( ! objList_.size() == 0 )
       {
         CommObjIFType * obj = objList_.back();
@@ -207,8 +207,8 @@ namespace Dune
       }
     }
 
-    //! add discrete function to communication list 
-    template <class DiscreteFunctionImp> 
+    //! add discrete function to communication list
+    template <class DiscreteFunctionImp>
     void addToList(DiscreteFunctionImp &df)
     {
       typedef DiscreteFunctionCommunicator<DiscreteFunctionImp> CommObjType;
@@ -216,14 +216,14 @@ namespace Dune
       objList_.push_back(obj);
     }
 
-    //! exchange discrete function to all procs we share data with 
-    //! by using given OperationImp when receiving data from other procs 
-    void exchange() 
+    //! exchange discrete function to all procs we share data with
+    //! by using given OperationImp when receiving data from other procs
+    void exchange()
     {
-      typedef CommObjListType :: iterator iterator; 
+      typedef CommObjListType :: iterator iterator;
       {
         iterator end = objList_.end();
-        for(iterator it = objList_.begin(); it != end; ++it) 
+        for(iterator it = objList_.begin(); it != end; ++it)
         {
           (*it)->exchange();
         }
@@ -232,9 +232,9 @@ namespace Dune
   };
 
   // end toggle AULGrid yes/no
-#endif 
+#endif
   //@}
-  
+
 } // end namespace Dune
 
 #endif
