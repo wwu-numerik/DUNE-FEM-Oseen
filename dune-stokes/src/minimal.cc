2c7f1b98082ec150665c0fc523a03de909be678c
Rene Milk <koshi@springlobby.info>
2009-10-06 23:55:46 +0200

    stuff bump

--- dune-stokes/src/minimal.cc
+++ dune-stokes/src/minimal.cc
@@ -1,14 +1,26 @@
 #include <dune/stuff/parametercontainer.hh>
-
+#include <dune/stuff/misc.hh>
 //do whatever you like to this file to test out simple and small stuff
 
 int main( int argc, char** argv ) {
 	if ( !(  Parameters().ReadCommandLine( argc, argv ) ) ) {
         return 1;
     }
+	const bool useLogger = true;
+    Logger().Create( Parameters().getParam( "loglevel",         62,                         useLogger ),
+                     Parameters().getParam( "logfile",          std::string("dune_stokes"), useLogger ),
+                     Parameters().getParam( "fem.io.logdir",    std::string(),              useLogger )
+                    );
+
 	std::vector<double> dummies = Parameters().getList( "list", 0.9 );
-	for ( std::vector<double>::const_iterator it = dummies.begin(); it != dummies.end(); ++it ) {
-		std::cout << *it << "\n";
-	}
+	Stuff::MinMaxAvg<double> nums ( dummies );
+	nums.output( std::cout );
+	nums.push( 	4.0 );
+	nums.output( std::cout );
+	nums.push( 12.0 );
+	nums.output( std::cout );
+	nums.push( 	4.0 );
+	nums.output( std::cout );
+
 	return 0;
 }
