06c9b8577bc0998974dcb81f64cbf7fc0013c5c6
Rene Milk <rene.milk@uni-muenster.de>
2010-04-28 10:10:24 +0200

    Revert "Use DGAdaptiveLeafGridpart as according to docs this is mandatory for adaption to work, nvm it's been working alright up till now" and with this change it doesn't anymore...
    This reverts commit 4922a633b142513733d41afcebc0710154eee3b7.

--- src/dune_stokes.cc
+++ src/dune_stokes.cc
@@ -499,7 +499,7 @@ RunInfo singleRun(  CollectiveCommunication& mpicomm,
         gridPtr->globalRefine( refine_level );
     }
 
-	typedef Dune::DGAdaptiveLeafGridPart< GridType >
+	typedef Dune::AdaptiveLeafGridPart< GridType >
         GridPartType;
     static GridPartType gridPart( *gridPtr );
 
