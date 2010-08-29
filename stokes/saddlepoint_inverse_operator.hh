58a4990b0e1b16df919cd91e72dfaaec93e57a63
Rene Milk <rene.milk@uni-muenster.de>
2010-08-29 16:19:19 +0200

    refactor common defines/types to a solver intercafe file

--- stokes/saddlepoint_inverse_operator.hh
+++ stokes/saddlepoint_inverse_operator.hh
@@ -1,54 +1,13 @@
 #ifndef DUNE_SPINVERSE_OPERATORS_HH
 #define DUNE_SPINVERSE_OPERATORS_HH
 
+#include <dune/stokes/solver_interface.hh>
+
 /** \file
     \brief SPinverseoperator_original.hh
  */
 
-// OEMBICGSQOp will NOT compile
-#ifndef INNER_CG_SOLVERTYPE
-    #define INNER_CG_SOLVERTYPE OEMCGOp
-#endif
-
-#ifndef OUTER_CG_SOLVERTYPE
-	#define OUTER_CG_SOLVERTYPE OEMCGOp
-#endif
-
-
-#ifdef USE_BFG_CG_SCHEME
-    #include <utility>
-    //< iteration no , < absLimit, residuum > >
-    typedef std::pair<int,std::pair<double,double> >
-        IterationInfo;
-	#include <dune/stokes/oemsolver/oemsolver.hh>
-	#define SOLVER_NAMESPACE DuneStokes
-#else
-	#include <dune/fem/solver/oemsolver/oemsolver.hh>
-	#define SOLVER_NAMESPACE Dune
-#endif
-
-#include <dune/fem/function/common/discretefunction.hh>
-#include <dune/fem/operator/matrix/spmatrix.hh>
-#include <dune/stokes/cghelper.hh>
-
-#include <dune/stuff/parametercontainer.hh>
-#include <dune/stuff/printing.hh>
-#include <dune/stuff/misc.hh>
-#include <dune/stuff/logging.hh>
-
-#include <cmath>
-
-
-
 namespace Dune {
-//! utility struct used to expose runtime statistics
-struct SaddlepointInverseOperatorInfo {
-	double iterations_inner_avg;
-    int iterations_inner_min;
-    int iterations_inner_max;
-    int iterations_outer_total;
-    double max_inner_accuracy;
-};
 
   /**   \brief Nested Conjugate Gradient Solver
         \tparam StokesPassImp discrete function types etc. get extracted from this
