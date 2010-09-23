cafeb4634370bc3639b329b6052758b5ce0d7fab
Rene Milk <rene.milk@uni-muenster.de>
2010-09-23 19:14:59 +0200

    back scale laplace to conatiner

--- stokes/stokespass.hh
+++ stokes/stokespass.hh
@@ -2135,6 +2135,7 @@ class StokesPass
 				Xmatrix.apply( rhs_datacontainer->velocity_gradient, velocity_tmp1 );
 				Ymatrix.apply( dest.discreteVelocity(), rhs_datacontainer->velocity_laplace );
 				rhs_datacontainer->velocity_laplace += velocity_tmp1;
+				rhs_datacontainer->velocity_laplace /= mu;
 
 				Omatrix.apply( dest.discreteVelocity(), rhs_datacontainer->convection );
 				rhs_datacontainer->convection += H2_O_rhs;//just in case I've switched fluxes and H2_O_rhs is then non-zero
