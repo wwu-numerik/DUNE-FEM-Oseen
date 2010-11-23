7ce5168bb0a3cf8e3fbf9ee577a948462ea1b6ae
Rene Milk <rene.milk@uni-muenster.de>
2010-11-23 18:50:54 +0100

    fix merge error

--- stokes/stokespass.hh
+++ stokes/stokespass.hh
@@ -1262,7 +1262,7 @@ class StokesPass
 										const VelocityRangeType xWorld_Outside = geometry.global( xOutside );
 										const LocalIntersectionCoordinateType xLocal = faceQuadratureNeighbour.localPoint( quad );
 										// get the integration factor
-										const double elementVolume = intersectionGeoemtry.integrationElement( xLocal );
+										const double elementVolume = intersectionGeometry.integrationElement( xLocal );
 										// get the quadrature weight
 										const double integrationWeight = faceQuadratureNeighbour.weight( quad );
 										const VelocityRangeType outerNormal = intersection.unitOuterNormal( xLocal );
